092d738d01582789c99f3bc936c916452acfc434
miroks404 <themiroks@gmail.com>
2024-11-15 17:08:33 +0200

    TODO-2 add data package

---
+++ app/src/main/java/ru/miroks404/todolistapp/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package ru.miroks404.todolistapp.data
+
+import ru.miroks404.todolistapp.domain.ShopItem
+import ru.miroks404.todolistapp.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItemById(shopItem.id)
+        shopList.remove(oldElement)
+        shopList.add(shopItem)
+    }
+
+    override fun getShopItemById(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId } ?: throw RuntimeException("Element not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+}
